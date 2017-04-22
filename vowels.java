65c442fe35fb646c93d4e9af540b06cb0beee6a1
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-04-22 11:16:13 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Scanner;
+public class vowels
+{
+public static void main(String mur[])
+{
+
+Scanner in=new Scanner(System.in);
+int m;
+System.out.println("Enter the Alphabet");
+m=in.nextInt();
+if(m=='a' || m=='e' || m=='i' || m=='o' || m=='u' || m=='A' ||m=='E' ||m=='I' ||m=='O' ||m=='U')
+{
+System.out.println("Enter the vowel");
+
+}
+else
+{
+System.out.println("Enter the constant");
+}
+ }
+}
