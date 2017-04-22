4bb3820b914d4ac1663a6bfc23147703da149b5a
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-04-22 11:12:28 +0530

    Create sakthi.java

---
+++ sakthi.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sakthi
+{
+public static void main(String mur[])
+{
+
+Scanner in=new Scanner(System.in);
+int a;
+a=in.nextInt();
+if(a%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+ }
+}
