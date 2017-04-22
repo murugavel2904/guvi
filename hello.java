21fc2f18c59da509de89a7691e5f197cb9d883ea
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-04-22 11:21:45 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+public class mur															
+{
+public static void main(String mur[])
+{
+for(int i=0;i<5;i++)
+System.out.println("Hello");
+}
+}
+}
