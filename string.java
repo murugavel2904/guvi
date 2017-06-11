da39e94b074da45bf8d2a473d07bd2f238a8ab09
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-06-12 01:02:22 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class string
+{
+    public static void main(String mur[])
+    {
+        Scanner in=new Scanner(System.in);
+       String str1=in.nextLine();
+       String str2=new StringBuffer(str1).reverse().toString();
+       int rev=Integer.parseInt(str2);
+      
+            System.out.print(()); 
+      
+      
+    }
+}
+
+
