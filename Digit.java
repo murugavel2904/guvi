7fdb45f0dbf558cbae6f4dd330c767119d4a364b
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-06-12 00:58:41 +0530

    Create Digit.java

---
+++ Digit.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Digit
+{
+    public static void main(String mur[])
+    {
+        Scanner in=new Scanner(System.in);
+       String str1=in.nextLine();
+       String str2=new StringBuffer(str1).reverse().toString();
+       int rev=Integer.parseInt(str2);
+      
+            System.out.print((rev)); 
+      
+      
+    }
+}
