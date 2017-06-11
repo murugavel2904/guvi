262b3e74051f49b2ec87cfc2ad425cc3d4ba0f64
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-06-12 00:41:04 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Reverse
+{
+    public static void main(String mur[])
+    {
+        Scanner in=new Scanner(System.in);
+       String str1=in.nextLine();
+       String str2=new StringBuffer(str1).reverse().toString();
+       System.out.print(str2);
+       
+       
+       
+    }
+}
