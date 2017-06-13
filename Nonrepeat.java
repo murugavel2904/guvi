cebf92baffc2300ecb2456f1b4200635dfd9c3a8
MURUGAVEL L <murugavelloganathan1996@gmail.com>
2017-06-13 12:07:38 +0530

    Create Nonrepeat.java

---
+++ Nonrepeat.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Nonrepeat {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int c=0;
+        int size=in.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+            arr[i]=in.nextInt();
+       // Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)
+        {   for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.println(""+arr[i]);
+                    
+                  
+                }}}for(int i=0;i<arr.length;i++)
+                {
+                    
+                
+                  if(arr[i]!=0)  
+                  {
+                    System.out.println(arr[i]);
+                }
+                }
+    }
+}
